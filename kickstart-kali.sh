6cbfbdda45969c741d7912769d1013d392260607
Elia <e.toaiari@gmail.com>
2022-07-24 13:33:19 -0400

    additions

--- kickstart-kali.sh
+++ kickstart-kali.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# updates the repositories
 sudo apt update
+
+# installs oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# installs various packages
 sudo apt install -y gobuster xclip ghidra hashid steghide
