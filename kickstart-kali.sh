650f8c1306f6c2d12b5122fdbcbc156127994dfb
et994 <55279734+et994@users.noreply.github.com>
2022-07-24 13:17:36 -0400

    Update kickstart-kali.sh

--- kickstart-kali.sh
+++ kickstart-kali.sh
@@ -2,5 +2,3 @@
 
 sudo apt update
 sudo apt install -y gobuster xclip ghidra hashid steghide
-
-wget -qO- https://raw.githubusercontent.com/et994/kali-config/main/kali-custom-profile-functions | sudo tee --append ~/.profile
