b58b5820303c97803bf7b814bbde41b645d96e36
et994 <55279734+et994@users.noreply.github.com>
2022-07-25 04:01:55 -0400

    Update kickstart-kali.sh

--- kickstart-kali.sh
+++ kickstart-kali.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # downloads .profile custom functions
-wget -qO- https://raw.githubusercontent.com/et994/kali-config/main/kali-custom-profile-functions | sudo tee --append ~/.profile
+wget -qO- https://raw.githubusercontent.com/et994/kali-config/main/kali-custom-profile-functions | sudo tee --append ~/.profile &>/dev/null
 
 # updates the repositories
-sudo apt update
+sudo apt update &>/dev/null
 
 # installs oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &>/dev/null
 
 # installs various packages
-sudo apt install -y gobuster xclip ghidra hashid steghide
+sudo apt install -y gobuster xclip ghidra hashid steghide &>/dev/null
